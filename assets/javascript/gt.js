9723163e6ef1044c4b3dd8d51446a3004aac0640
albert <albert.factora@gmail.com>
2017-05-30 16:55:08 -0700

    Added paging for consecutive same button clicked.

--- assets/javascript/gt.js
+++ assets/javascript/gt.js
@@ -1,5 +1,6 @@
-var gifs = ["basketball", "baseball"];
-page = 0; //- paging option
+var gifs = ["basketball", "baseball"]; //- seed gifs
+var offset = 0; //- paging option
+var lastButton = "";
 
 $("#add-gif").on("click", function (event){
 	event.preventDefault(); //- this is necessary or array.push will not work 
@@ -31,32 +32,56 @@ function fetch() {
 	// var queryURL = "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=40e9cece";
 	// var queryURL = "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC"
 	var fetchItem = $(this).attr("data-name");
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + fetchItem.replace(" ", "+") + "&api_key=dc6zaTOxFJmzC";
-	var arrResults = [];
-	// console.log(queryURL);
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + fetchItem.replace(" ", "+") + "&api_key=dc6zaTOxFJmzC&offset=" + offset;
+	var arrAdded = [];
+	var arrOne = 0;
+console.log(queryURL);
 
 	$.ajax({
 	  url: queryURL,
 	  method: "GET"
 	}).done(function(response) {
+		// console.log(response.data.length);
+		// for (j=0; j<response.data.length; j++) {
+		// 	var callBack = '{' + '"id: "' + response.data[j].id + '", "rating: "' +
+		// 		response.data[j].rating + '", gif: "' + response.data[0].images.fixed_height.url + '"}';
+		// 	if (j !== response.data.length -1) {
+		// 				var terminator = ',';
+		// 			} else {
+		// 				var terminator = '';
+		// 			}
+		// 	var arrResultsItems = callBack + terminator;
 
-	  // console.log(response.images.source)
-	  // console.log(response.Runtime);
-	  // console.log("source: " + response.data[0].source);
+		// 	// arrResults.push(callBack);
+		// 	console.log(arrResultsItems);
+		// }
+		// while (arrOne < 25) {
+		for (j=0; j<response.data.length; j++) {
+		// $("#api-results").append('<div class="text">rating: ' + response.data[j].rating + '</div>');
+		$("#api-results").prepend('<img class="image" src="' + response.data[j].images.fixed_height.url + '">');
+		// $("#api-results").prepend('<div class="the-gif"><img src="' + response.data[j].images.fixed_height.url + '"></div>');
+		// $("#api-results").prepend('<img src="' + response.data[j].images.fixed_height.url + '" alt="Rating: ' + response.data[j].rating + '">');
+		
+		// arrOne++;
+		console.log(response.data[j].id);
+		}
+		if (lastButton === "" || (lastButton !== "" && lastButton === fetchItem)) {
+			offset = offset + 25;
+		} else if (lastButton !== "" && lastButton !== fetchItem) {
+			offset = 0;
+		}
+		lastButton = fetchItem;
+		console.log(offset);
+		// nextClick(); //- prepend the next gif from the same button
 
-	  // console.log(response);
-	  // console.log("rating: " + response.data[0].rating);
-	  // console.log(JSON.stringify(response.data[0].images.looping));
-	  // console.log('<img src="' + response.data[0].images.fixed_height.url + '">');
-	  // console.log("total results: " + response.pagination.total_count);
-	  console.log("prior to showing");
-	  $("#api-results").prepend('<div class="text">rating: ' + response.data[0].rating + '</div>');
-	  $("#api-results").prepend('<div class="the-gif"><img src="' + response.data[0].images.fixed_height.url + '"></div>');
-	  console.log("after showing");
-	});
+	}); //end ajax
 }
+
+// function nextClick()  {
+// 	console.log("second function");
+// }
 
 $(document).on("click", ".gif", fetch);
 
-addGifButtons(); //- seed the pages with a few buttons already in the gif array
+addGifButtons(); //- seed the page with a few buttons already in the gif array
 // ---------------------------------------------------------
